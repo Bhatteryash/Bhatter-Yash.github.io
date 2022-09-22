298eadd9feca11dcca1f7b79e4f73f5ee250833b
Yash Bhatter <yashbhatter.yb@gmail.com>
2022-09-22 19:05:23 +0530

    Github calender added

--- index.js
+++ index.js
@@ -38,9 +38,6 @@ closemenu=()=>{
 
 
 
-
-
-
   const scriptURL = 'https://script.google.com/macros/s/AKfycbxikQI-lHaHmDgM8jOrq1aXC-9aLdMrGUPqWZeuw1GshvdLAZfz294EJZxsi0MuG3_H/exec'
   const form = document.forms['submit-to-google-sheet']
   const msg=document.getElementById("msg");
