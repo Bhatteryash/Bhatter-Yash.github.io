1d96a5c24e0610d12b6dce6e51148b23a86a6634
Yash Bhatter <yashbhatter.yb@gmail.com>
2022-11-15 16:34:42 +0530

    Github updated

--- index.js
+++ index.js
@@ -8,6 +8,16 @@
 //   })
   
 
+document.getElementById("res1").addEventListener("click",()=>{
+window.open("https://drive.google.com/file/d/10rOCMW-zp04idDhWGqmHBIGYQaGusKML/view?usp=sharing");
+})
+
+document.getElementById("res2").addEventListener("click",()=>{
+    window.open("https://drive.google.com/file/d/10rOCMW-zp04idDhWGqmHBIGYQaGusKML/view?usp=sharing");
+    })
+
+
+
 let details=document.getElementsByClassName("skill-ed");
 let content=document.getElementsByClassName("contents");
 
@@ -58,3 +68,50 @@ window.addEventListener("scroll",function(){
     let nav=this.document.querySelector("nav");
     nav.classList.toggle("sticky",window.scrollY > 0);
 })
+ 
+let container=  document.getElementById("skill_container");
+ 
+let skill_arr=[
+    "./images/java.png",
+    "./images/springboot.png",
+    "./images/hibernate.png",
+    "./images/sql.jpg",
+    "./images/js.png",
+    "./images/html.png",
+    "./images/css.png",
+    "./images/aws.png",
+    "./images/github.png",
+    "./images/netlify.png"
+];
+let skill_name=[
+    "Java",
+    "SpringBoot",
+    "Hibernate",
+    "SQL",
+    "JavaScript",
+    "HTML",
+    "CSS",
+    "AWS",
+    "GitHub",
+    "Netlify"
+]
+let count=0;
+AppendSkills=(arr)=>{
+container.innerHTML=null;
+    arr.forEach((ele)=>{
+        let div= document.createElement("div");
+        let img=document.createElement("img");
+        img.src=ele;
+        let name=document.createElement("h2");
+        name.innerText=skill_name[count];
+        name.style.color="white";
+        count++;
+        div.append(img,name);
+        container.append(div);
+    })
+}
+
+window.addEventListener("load",()=>{
+    AppendSkills(skill_arr);
+})
+
